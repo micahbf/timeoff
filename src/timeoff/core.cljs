87dc6ee85832a9188acb74b2dc15d10add066571
Micah Buckley-Farlee <micah@xlmpq.com>
2020-07-25 13:15:57 -0500

    Start with form elements

--- src/timeoff/core.cljs
+++ src/timeoff/core.cljs
@@ -1,13 +1,45 @@
 (ns timeoff.core
     (:require
       [reagent.core :as r]
-      [reagent.dom :as d]))
+      [reagent.dom :as d]
+      [cljs-time.core :as time]))
 
 ;; -------------------------
 ;; Views
 
+(defonce hours-accrued (r/atom 0))
+(defonce future-days-used (r/atom 0))
+(defonce accrued-as-of (r/atom (time/today)))
+
+(defn input-element [id type value-fn on-change]
+  [:input {:id id
+           :class "form-control"
+           :type type
+           :value (value-fn atom)
+           :on-change on-change}])
+
+(defn basic-input
+  ([id label input-type atom]
+   (basic-input id label input-type atom #(reset! atom (-> % .-target .-value))))
+  ([id label input-type atom on-change]
+   [:div.form-group
+    [:label {:for id} label]
+    [input-element id input-type #(deref atom) on-change]]))
+
+(defn date-input [id label atom]
+  [:div.form-group
+   [:label {:for id} label]
+   [input-element id "date" atom]])
+
+(defn hours-accrued-input []
+  (basic-input "hours-accrued" "Hours accrued" "number" hours-accrued))
+
+(defn future-days-used-input []
+  (basic-input "future-used" "Future days used" "number" future-days-used))
+
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  [:div [:h2 "Time off calculator"]]
+  [:form [hours-accrued-input] [future-days-used-input]])
 
 ;; -------------------------
 ;; Initialize app
